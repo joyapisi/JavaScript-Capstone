dae666bdcf5b0def3b42b34ffca4965fb1abae18
Joy Phoebe <joyapisi@gmail.com>
2023-05-03 08:37:24 +0300

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import fetchData from './modules/displayItems.js';
 
 fetchData();
-const logo = require('../sources/mylogo.png');
+const logo = require('../sources/logo.png');
 
 const img = document.getElementById('img');
 img.src = logo;
